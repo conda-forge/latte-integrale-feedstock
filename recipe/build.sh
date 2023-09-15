88ae22002e9b8e8ac051fff391007de93fe983ae
Julian Rüth <julian.rueth@fsfe.org>
2023-09-15 14:18:13 +0200

    Only build shared library
    Since ArchLinux sets --enable-shared, this probably does something here

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,7 @@
 # We can not use C++17 due to https://stackoverflow.com/questions/47284705/c1z-dynamic-exception-specification-error
 export CXXFLAGS="$CXXFLAGS -std=c++14"
 
-./configure --prefix=$PREFIX --with-ntl=$PREFIX --with-cddlib=$PREFIX || (cat config.log; false)
+./configure --prefix=$PREFIX --enable-shared --disable-static --with-ntl=$PREFIX --with-cddlib=$PREFIX || (cat config.log; false)
 make -j${CPU_COUNT}
 make install
 make check || (cat code/test-suite/test-suite.log; false)
